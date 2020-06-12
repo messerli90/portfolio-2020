8f6c454a481249d9f77b25444f2b92450c9d16d5
Michael Messerli <michaelamesserli@gmail.com>
2020-06-11 21:37:31 -0500

    :recycle: Refactor store to use repositories

---
+++ repositories/ProjectRepository.js
@@ -0,0 +1,26 @@
+export default {
+  all() {
+    return projects
+  }
+}
+
+const projects = [
+  {
+    title: 'JobHuntBuddy',
+    slug: 'jobhuntbuddy',
+    url: 'https://jobhuntbuddy.com',
+    description:
+      'Weekend Challenge. JobHuntBuddy is a web app helping job hunters manage the application process.',
+    image: '/images/jobhuntbuddy_2.png',
+    type: 'webapp'
+  },
+  {
+    title: 'Siegrs.GG',
+    slug: 'siegrs',
+    url: 'https://siegrs.gg',
+    description:
+      'Platform for players of Rainbow Six: Siege to browse other players and teams. Serving over 30k players',
+    image: '/images/siegrs_banner_2.png',
+    type: 'webapp'
+  }
+]
